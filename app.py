c22108d9524e1a535241f8b01042136f235db517
shuhaili <shuhaili@tmrnd.com.my>
2021-11-19 02:50:32 +0800

    updated

--- app.py
+++ app.py
@@ -6,14 +6,33 @@ Created on Thu Nov 18 23:35:07 2021
 """
 
 # Creating GUI with tkinter
-from chatgui import *
-import tkinter
-from tkinter import *
+from chatgui import clean_up_sentence, bow, predict_class, getResponse, chatbot_response
 import streamlit as st
 
 
-'''
-# Chatbot
-## Thank you for contact us and have a good day!
-'''
+# '''
+# # Chatbot
+# ## Thank you for contact us and have a good day!
+# '''
 
+
+def get_text():
+    input_text = st.text_input("You: ","")
+    return input_text
+    
+
+st.title("""
+NLP Bot  
+NLP Bot is an NLP conversational chatterbot. Initialize the bot by clicking the "Send" button. 
+""")
+
+st.title("Your bot is ready to talk to you")
+
+        
+user_input = get_text()
+
+ind = 1
+if st.button('Send'):    
+    ind = ind +1
+
+st.text_area("Bot:", value=chatbot_response(user_input), height=200, max_chars=None, key=None)
--- chatgui.py
+++ chatgui.py
@@ -1,12 +1,11 @@
-import streamlit as st
 import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 # Creating GUI with tkinter
-import tkinter
-from tkinter import *
+# import tkinter
+# from tkinter import *
 from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
@@ -69,50 +68,50 @@ def chatbot_response(msg):
 # res = chatbot_response(msg)
 
 
-def send():
-    msg = EntryBox.get("1.0",'end-1c').strip()
-    EntryBox.delete("0.0",END)
+# def send():
+#     msg = EntryBox.get("1.0",'end-1c').strip()
+#     EntryBox.delete("0.0",END)
 
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+#     if msg != '':
+#         ChatLog.config(state=NORMAL)
+#         ChatLog.insert(END, "You: " + msg + '\n\n')
+#         ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
     
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
+#         res = chatbot_response(msg)
+#         ChatLog.insert(END, "Bot: " + res + '\n\n')
             
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
+#         ChatLog.config(state=DISABLED)
+#         ChatLog.yview(END)
  
 
-base = Tk()
-base.title("Hello")
-base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
+# base = Tk()
+# base.title("Hello")
+# base.geometry("400x500")
+# base.resizable(width=FALSE, height=FALSE)
 
-#Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+# #Create Chat window
+# ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
-ChatLog.config(state=DISABLED)
+# ChatLog.config(state=DISABLED)
 
-#Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
+# #Bind scrollbar to Chat window
+# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+# ChatLog['yscrollcommand'] = scrollbar.set
 
-#Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-                    command= send )
+# #Create Button to send message
+# SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
+#                     bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+#                     command= send )
 
-#Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
+# #Create the box to enter message
+# EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+# #EntryBox.bind("<Return>", send)
 
 
-#Place all components on the screen
-scrollbar.place(x=376,y=6, height=386)
-ChatLog.place(x=6,y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+# #Place all components on the screen
+# scrollbar.place(x=376,y=6, height=386)
+# ChatLog.place(x=6,y=6, height=386, width=370)
+# EntryBox.place(x=128, y=401, height=90, width=265)
+# SendButton.place(x=6, y=401, height=90)
 
-base.mainloop()
+# base.mainloop()
