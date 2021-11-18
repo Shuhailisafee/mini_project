b3793b89c98bf60baa6b6ddc71535a7f2e3778fa
shuhaili <shuhaili@tmrnd.com.my>
2021-11-19 01:03:31 +0800

    my_code

---
+++ README.md
@@ -0,0 +1,11 @@
+# chatbot_mini_project
+
+A chatbot is an intelligent piece of software that is capable of communicating and performing actions similar to a human. 
+
+## Python library
+Install the dependencies from requirements.txt
+
+## Run the program
+```sh
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 18 23:35:07 2021
+
+@author: shuhaili
+"""
+
+# Creating GUI with tkinter
+from chatgui import *
+import tkinter
+from tkinter import *
+import streamlit as st
+
+
+'''
+# Chatbot
+## Thank you for contact us and have a good day!
+'''
+
